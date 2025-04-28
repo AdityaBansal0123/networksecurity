967f4c001d19cdf68189a48f0c81d857fd4e8b5f
Aditya Bansal <bansaladitya048@gmail.com>
2025-04-28 21:19:31 +0530

    Fixed dagshub init issue in ModelTrainer

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -15,8 +15,11 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 import mlflow
-import dagshub
-dagshub.init(repo_owner='AdityaBansal0123', repo_name='networksecurity', mlflow=True)
+
+# Only initialize dagshub if DISABLE_DAGSHUB is not set
+if not os.getenv("DISABLE_DAGSHUB"):
+    import dagshub
+    dagshub.init(repo_owner='AdityaBansal0123', repo_name='networksecurity', mlflow=True)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -82,28 +85,24 @@ class ModelTrainer:
             },
         }
 
-        model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=x_test,y_test=y_test,
+        model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=x_test,y_test=y_test,
                                           models=models,params=params)
         
         ## To get best model score from dict
         best_model_score = max(sorted(model_report.values()))
 
         ## To get best model name from dict
-
-        best_model_name = list(model_report.keys())[
-            list(model_report.values()).index(best_model_score)
-        ]
+        best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
         best_model = models[best_model_name]
+        
         y_train_pred=best_model.predict(X_train)
-
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
+        
         y_test_pred=best_model.predict(x_test)
-
-        ## Track the experiements with mlflow
-        self.track_mlflow(best_model,classification_train_metric)
-        
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
-        
+
+        ## Track the experiments with mlflow
+        self.track_mlflow(best_model,classification_train_metric)
         self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)            
@@ -111,25 +110,27 @@ class ModelTrainer:
         os.makedirs(model_dir_path,exist_ok=True)
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
-        save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
-        #model pusher
-        save_object("final_model/model.pkl",best_model)
+        save_object(self.model_trainer_config.trained_model_file_path,obj=Network_Model)
         
-
+        # model pusher
+        save_object("final_model/model.pkl", best_model)
+        
         ## Model Trainer Artifact 
-        model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
-                             train_metric_artifact=classification_train_metric,
-                             test_metric_artifact=classification_test_metric
-                             )
+        model_trainer_artifact=ModelTrainerArtifact(
+            trained_model_file_path=self.model_trainer_config.trained_model_file_path,
+            train_metric_artifact=classification_train_metric,
+            test_metric_artifact=classification_test_metric
+        )
+        
         logging.info(f"Model trainer artifact: {model_trainer_artifact}")
         return model_trainer_artifact
         
-    def initiate_model_trainer(self)->ModelTrainerArtifact:
+    def initiate_model_trainer(self) -> ModelTrainerArtifact:
         try:
             train_file_path = self.data_transformation_artifact.transformed_trained_file_path
             test_file_path = self.data_transformation_artifact.transformed_test_file_path
 
-            #loading training array and testing array
+            # loading training array and testing array
             train_arr = load_numpy_array_data(train_file_path)
             test_arr = load_numpy_array_data(test_file_path)
 
